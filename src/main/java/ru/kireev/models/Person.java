2927b71ac21318e5fe81ec0bcbcdf7406ae64db3
temak22 <kireev.a.o@edu.mirea.ru>
2022-08-05 12:08:00 +0300

    Project on List<>

---
+++ src/main/java/ru/kireev/models/Person.java
@@ -0,0 +1,37 @@
+package ru.kireev.models;
+
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name's length should from 2 to 30")
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
